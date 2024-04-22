89bd34867b3897af48acbc94c246c3b52bf0400e
TheMaZe <shitov.1497@bk.ru>
2024-04-22 18:13:54 +0300

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
+    server_socket = socket.create_server(("localhost", 4221))
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    conn, addr = server_socket.accept() # wait for client
+    http_response = b'HTTP/1.1 200 OK\r\n\r\n'
+    conn.send(http_response)
+    conn.close()
 
 if __name__ == "__main__":
     main()
