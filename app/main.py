28501494dbef4029b95cfad60b6e2aa565ee3d15
TheMaZe <shitov.1497@bk.ru>
2024-04-25 11:31:14 +0300

    paste 3rd stages

--- app/main.py
+++ app/main.py
@@ -6,8 +6,17 @@ def main():
     server_socket = socket.create_server(("localhost", 4221))
 
     conn, addr = server_socket.accept() # wait for client
-    http_response = b'HTTP/1.1 200 OK\r\n\r\n'
-    conn.send(http_response)
+    request_client = conn.recv(1024).decode()
+    method_http, path, version = request_client.split('\r\n')[0].split()
+
+    ok_res = b"HTTP/1.1 200 OK\r\n\r\n"
+    not_found_res = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+    if path == '/':
+        conn.send(ok_res)
+    else:
+        conn.send(not_found_res)
+    
     conn.close()
 
 if __name__ == "__main__":
