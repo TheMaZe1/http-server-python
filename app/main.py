68c6963d6321156c4c74265258a97416e9b82658
TheMaZe <shitov.1497@bk.ru>
2024-04-25 16:57:04 +0300

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def client_handler(connection, address):
     request_client = connection.recv(1024).decode()
     headers = request_client.split('\r\n')
     method_http, path, version = headers[0].split()
-    user_agent = headers[2].split()[1]
 
     if path == '/':
         response = b'HTTP/1.1 200 OK\r\n\r\n'
@@ -15,6 +14,7 @@ def client_handler(connection, address):
         headers_respone = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(body_response)}\r\n\r\n'.encode()
         response = headers_respone + body_response
     elif 'user-agent' in path:
+        user_agent = headers[2].split()[1]
         headers_respone = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n'.encode()
         response = headers_respone + f'{user_agent}'.encode()
     else:
