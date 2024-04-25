f79a53800dba6dd4ed9bfe606ad257443c6a5d58
TheMaZe <shitov.1497@bk.ru>
2024-04-25 17:21:45 +0300

    pass 7th stage

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import socket
 from threading import Thread
+import os
+import sys
 
-def client_handler(connection, address):
+def client_handler(connection, address, dir=''):
 
     request_client = connection.recv(1024).decode()
     headers = request_client.split('\r\n')
@@ -17,6 +19,15 @@ def client_handler(connection, address):
         user_agent = headers[2].split()[1]
         headers_respone = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n'.encode()
         response = headers_respone + f'{user_agent}'.encode()
+    elif 'files' in path:
+        filename = path[len('/files/'):].encode()
+        if os.path.exists(dir + '/' + filename):
+            with open(dir + '/' + filename, 'rb') as f:
+                data = f.read()
+                headers_respone = f'HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(data)}\r\n\r\n'.encode()
+                response = headers_respone + data
+        else:
+            response = b'HTTP/1.1 404 Not Found\r\n\r\n'
     else:
         response = b'HTTP/1.1 404 Not Found\r\n\r\n'
 
@@ -27,10 +38,16 @@ def client_handler(connection, address):
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221))
+    dir = ''
+    if len(sys.argv) > 2:
+        dir = sys.argv[2]
 
     while True:
         conn, addr = server_socket.accept() # wait for client
-        thread = Thread(target=client_handler, args=(conn, addr))
+        if dir:
+            thread = Thread(target=client_handler, args=(conn, addr, dir))
+        else:
+            thread = Thread(target=client_handler, args=(conn, addr))
         thread.start()
 
 if __name__ == "__main__":
